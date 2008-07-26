73346ca12c53cbaee7d252c1e1e7ea2d7233d600
Krishnendu Chatterjee <c_krish@awela.(none)>
2008-07-25 21:28:06 -0700

    Changed format_strings statements to Printf.sprintf statements (except one).

--- online/analysis/online_db.ml
+++ online/analysis/online_db.ml
@@ -125,15 +125,12 @@ class db
     (* Global methods. *)
 
     val sth_update_histogram = "UPDATE wikitrust_global SET median = ?, rep_0 = ?, rep_1 = ?, rep_2 = ?, rep_3 = ?, rep_4 = ?, rep_5 = ?, rep_6 = ?, rep_7 = ?, rep_8 = ?, rep_9 =?"
-    val sth_select_histogram = "SELECT * FROM wikitrust_global"
-    val sth_select_last_colored_rev = "SELECT A.revision_id, B.rev_page, A.coloredon FROM wikitrust_colored_markup AS A JOIN revision AS B ON (A.revision_id = B.rev_id) ORDER BY coloredon DESC LIMIT 1"
-    val sth_select_all_revs_after = "SELECT rev_id, rev_page, rev_text_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision WHERE rev_timestamp > ? ORDER BY rev_timestamp ASC"      
-    val sth_select_all_revs = "SELECT rev_id, rev_page, rev_text_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision ORDER BY rev_timestamp ASC"
 
     (** [get_histogram] Returns a histogram showing the number of users 
 	at each reputation level, and the median. *)
     method get_histogram : float array * float =
-      match fetch (db_exec dbh sth_select_histogram) with
+    let s = Printf.sprintf "SELECT * FROM wikitrust_global" in
+      match fetch (db_exec dbh s) with
         | None -> raise DB_Not_Found
         | Some row -> ([| not_null float2ml row.(1); not_null float2ml row.(2); not_null float2ml row.(3); 
 			  not_null float2ml row.(4);  not_null float2ml row.(5);
@@ -151,20 +148,23 @@ class db
 
     (** Returns the last colored revision, if any *)
     method fetch_last_colored_rev : (int * int * timestamp_t) = 
-      match fetch (db_exec dbh sth_select_last_colored_rev) with
+    let s =  Printf.sprintf "SELECT A.revision_id, B.rev_page, A.coloredon FROM wikitrust_colored_markup AS A JOIN revision AS B ON (A.revision_id = B.rev_id) ORDER BY coloredon DESC LIMIT 1" in
+      match fetch (db_exec dbh s) with
         | None -> raise DB_Not_Found
         | Some row -> (not_null int2ml row.(0), not_null int2ml row.(1), 
             not_null timestamp2ml row.(2))
   
     (** [sth_select_all_revs_after (int * int * int * int * int * int)] returns all 
         revs created after the given timestamp. *)
-    method fetch_all_revs_after (timestamp : timestamp_t) : Mysql.result =   
-      db_exec dbh (format_string sth_select_all_revs_after [ml2timestamp timestamp])
+    method fetch_all_revs_after (timestamp : timestamp_t) : Mysql.result =  
+      let s = Printf. sprintf "SELECT rev_id, rev_page, rev_text_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision WHERE rev_timestamp > %s ORDER BY rev_timestamp ASC" (ml2timestamp timestamp) in  
+      db_exec dbh s
 
     (** [fetch_all_revs] returns a cursor that points to all revisions in the database, 
 	in ascending order of timestamp. *)
     method fetch_all_revs : Mysql.result = 
-      db_exec dbh (format_string sth_select_all_revs [])
+      let s= Printf.sprintf  "SELECT rev_id, rev_page, rev_text_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision ORDER BY rev_timestamp ASC"  in
+      db_exec dbh s 
   
     (* ================================================================ *)
     (* Page methods. *)
@@ -206,51 +206,43 @@ class db
     (* ================================================================ *)
     (* Revision methods. *)
 
-    val sth_select_revision_info = "SELECT quality_info FROM wikitrust_revision WHERE revision_id = ?"
-    val sth_delete_revision_info = "DELETE FROM wikitrust_revision WHERE revision_id = ?"
-    val sth_insert_revision_info = "INSERT INTO wikitrust_revision (revision_id, quality_info, reputation_delta, overall_trust) VALUES (?, ?, ?, ?)"
-    val sth_delete_markup = "DELETE FROM wikitrust_colored_markup WHERE revision_id = ?"      
-    val sth_insert_markup = "INSERT INTO wikitrust_colored_markup (revision_id, revision_text) VALUES (?, ?)" 
-    val sth_select_markup = "SELECT revision_text FROM wikitrust_colored_markup WHERE revision_id = ?" 
-    val sth_select_author_sigs = "SELECT sigs FROM wikitrust_sigs WHERE revision_id = ?"
-    val sth_insert_author_sigs = "INSERT INTO wikitrust_sigs (revision_id, sigs) VALUES (?, ?)"
-    val sth_delete_author_sigs = "DELETE FROM wikitrust_sigs WHERE revision_id = ?"
-    val sth_select_rev_timestamp = "SELECT rev_timestamp FROM revision WHERE rev_id = ?"
-    val sth_select_text = "SELECT old_text FROM text WHERE old_id = ?"
-    val sth_select_rev_by_id = "SELECT rev_id, rev_page, rev_text_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision WHERE rev_id = ?"
-
     (** [read_revision rev_id] reads a revision by id, returning the row *)
-    method read_revision (rev_id: int) : string option array option =
-      fetch (db_exec dbh (format_string sth_select_rev_by_id [ml2int rev_id]))
+    method read_revision (rev_id: int) : string option array option = 
+      let s = Printf.sprintf "SELECT rev_id, rev_page, rev_text_id, rev_timestamp, rev_user, rev_user_text, rev_minor_edit, rev_comment FROM revision WHERE rev_id = %s" (ml2int rev_id) in
+      fetch (db_exec dbh s)
 
     (** [write_revision_info rev_id quality_info elist] writes the wikitrust data 
 	associated with revision with id [rev_id] *)
     method write_revision_info (rev_id: int) (quality_info: qual_info_t) : unit = 
-      ignore (db_exec dbh (format_string sth_delete_revision_info [ml2int rev_id]));
-      ignore (db_exec dbh (format_string sth_insert_revision_info [
-	ml2int rev_id;
-	ml2str (string_of__of__sexp_of sexp_of_qual_info_t quality_info); 
-	ml2float quality_info.reputation_gain; 
-	ml2float quality_info.overall_trust
-      ]))
+      let s1 =  Printf.sprintf "DELETE FROM wikitrust_revision WHERE revision_id = %s" (ml2int rev_id) in
+      let s2 =  Printf.sprintf "INSERT INTO wikitrust_revision (revision_id, quality_info, reputation_delta, overall_trust) VALUES (%s, %s, %s, %s)"
+ 	(ml2int rev_id)
+	(ml2str (string_of__of__sexp_of sexp_of_qual_info_t quality_info) ) 
+	(ml2float quality_info.reputation_gain) 
+	(ml2float quality_info.overall_trust) in
+      ignore (db_exec dbh s1);
+      ignore (db_exec dbh s2)
 
     (** [read_revision_info rev_id] reads the wikitrust information of revision_id *)
     method read_revision_info (rev_id: int) : qual_info_t = 
-      let result = db_exec dbh (format_string sth_select_revision_info [ml2int rev_id]) in
+      let s = Printf.sprintf "SELECT quality_info FROM wikitrust_revision WHERE revision_id = %s" (ml2int rev_id) in 
+      let result = db_exec dbh s in
       match fetch result with
         | None -> raise DB_Not_Found
         | Some x -> of_string__of__of_sexp qual_info_t_of_sexp (not_null str2ml x.(0))
 
    (** [fetch_rev_timestamp rev_id] returns the timestamp of revision [rev_id] *)
     method fetch_rev_timestamp (rev_id: int) : timestamp_t = 
-      let result = db_exec dbh (format_string sth_select_rev_timestamp [ml2int rev_id]) in 
+      let s = Printf.sprintf  "SELECT rev_timestamp FROM revision WHERE rev_id = %s" (ml2int rev_id) in 
+      let result = db_exec dbh s in 
       match fetch result with 
 	None -> raise DB_Not_Found
       | Some row -> not_null timestamp2ml row.(0)
 
     (** [get_rev_text text_id] returns the text associated with text id [text_id] *)
-    method read_rev_text (text_id: int) : string = 
-      let result = db_exec dbh (format_string sth_select_text [ml2int text_id]) in 
+    method read_rev_text (text_id: int) : string =
+      let s = Printf.sprintf "SELECT old_text FROM text WHERE old_id = %s" (ml2int text_id) in
+      let result = db_exec dbh s in 
       match Mysql.fetch result with 
         | None -> raise DB_Not_Found
         | Some y -> not_null str2ml y.(0)
@@ -267,18 +259,18 @@ class db
 	may be highly advisable. *)
     (* This is currently a first cut, which will be hopefully optimized later *)
     method write_colored_markup (rev_id : int) (markup : string) : unit =
-      ignore (db_exec dbh (format_string sth_delete_markup
-          [ml2int rev_id ]));
-      ignore (db_exec dbh (format_string sth_insert_markup 
-          [ml2int rev_id; ml2str markup ]));
+      let s1 = Printf.sprintf "DELETE FROM wikitrust_colored_markup WHERE revision_id = %s" (ml2int rev_id) in 
+      let s2 = Printf.sprintf "INSERT INTO wikitrust_colored_markup (revision_id, revision_text) VALUES (%s, %s)" (ml2int rev_id) (ml2str markup) in 
+      ignore (db_exec dbh s1); 
+      ignore (db_exec dbh s2);
       if commit_frequently then ignore (db_exec dbh "COMMIT")
 
     (** [read_colored_markup rev_id] reads the text markup of a revision with id
 	[rev_id].  The markup is the text of the revision, annontated with trust
 	and origin information. *)
     method read_colored_markup (rev_id : int) : string =
-      let result = db_exec dbh (format_string sth_select_markup 
-          [ml2int rev_id]) in
+      let s = Printf.sprintf  "SELECT revision_text FROM wikitrust_colored_markup WHERE revision_id = %s" (ml2int rev_id) in 
+      let result = db_exec dbh s in
       match Mysql.fetch result with
         | None -> raise DB_Not_Found
         | Some x -> not_null str2ml x.(0)
@@ -289,10 +281,10 @@ class db
       (sigs: Author_sig.packed_author_signature_t array) : unit = 
       let g = sexp_of_array Author_sig.sexp_of_sigs in 
       let s = string_of__of__sexp_of g sigs in 
-      ignore (db_exec dbh (format_string sth_delete_author_sigs 
-	[ml2int rev_id])); 
-      ignore (db_exec dbh (format_string sth_insert_author_sigs
-	[ml2int rev_id; ml2str s])) 
+      let s1 = Printf.sprintf "DELETE FROM wikitrust_sigs WHERE revision_id = %s" (ml2int rev_id) in 
+      let s2 = Printf.sprintf "INSERT INTO wikitrust_sigs (revision_id, sigs) VALUES (%s, %s)" (ml2int rev_id) (ml2str s) in 
+      ignore (db_exec dbh s1 ); 
+      ignore (db_exec dbh s2 )
 
       (** [read_author_sigs rev_id] reads the author signatures for the revision 
 	[rev_id]. 
@@ -305,8 +297,8 @@ class db
         *)
     method read_author_sigs 
       (rev_id: int) : Author_sig.packed_author_signature_t array = 
-      let result = db_exec dbh (format_string sth_select_author_sigs 
-          [ml2int rev_id ]) in 
+      let s = Printf.sprintf  "SELECT sigs FROM wikitrust_sigs WHERE revision_id = %s" (ml2int rev_id) in 
+      let result = db_exec dbh s in 
       match Mysql.fetch result with 
 	None -> raise DB_Not_Found
       | Some x -> begin
@@ -315,28 +307,25 @@ class db
 	end
 
    (** [delete_author_sigs rev_id] removes from the db the author signatures for [rev_id]. *)
-    method delete_author_sigs (rev_id: int) : unit = 
-      ignore (db_exec dbh (format_string sth_delete_author_sigs [ml2int rev_id]))
+    method delete_author_sigs (rev_id: int) : unit =
+      let s = Printf.sprintf  "DELETE FROM wikitrust_sigs WHERE revision_id = %s" (ml2int rev_id) in  
+      ignore (db_exec dbh s)
 
     (* ================================================================ *)
     (* User methods. *)
-
-    val sth_select_user_rep = "SELECT user_rep FROM wikitrust_user WHERE user_id = ?" 
-    val sth_update_user_rep = "UPDATE wikitrust_user SET user_rep = ? WHERE user_id = ?" 
-    val sth_insert_user_rep = "INSERT INTO wikitrust_user (user_id,  user_rep) VALUES (?, ?)" 
 
     (** [set_rep uid r] sets, in the table relating user ids to reputations, 
 	  the reputation of user [uid] to be equal to [r]. *)
     method set_rep (uid : int) (rep : float) =
       (* first check to see if there exists the user already *)
       try
+        let s =  Printf.sprintf "UPDATE wikitrust_user SET user_rep = %s WHERE user_id = %s"  (ml2float rep) (ml2int uid ) in
         ignore (self#get_rep uid ) ;
-        ignore (db_exec dbh (format_string sth_update_user_rep 
-            [ml2float rep; ml2int uid ])); 
+        ignore (db_exec dbh s); 
       with
-        DB_Not_Found -> 
-          ignore (db_exec dbh (format_string sth_insert_user_rep 
-              [ml2int uid; ml2float rep ]));  
+        DB_Not_Found ->
+          let s = Printf.sprintf  "INSERT INTO wikitrust_user (user_id,  user_rep) VALUES (%s, %s)"  (ml2int uid)  (ml2float rep ) in
+          ignore (db_exec dbh s);  
       if commit_frequently then ignore (db_exec dbh "COMMIT")
   
     (** [get_rep uid] gets the reputation of user [uid], from a table 
@@ -344,7 +333,8 @@ class db
         @raise DB_Not_Found if no tuple is returned by the database.
     *)
     method get_rep (uid : int) : float =
-      let result = db_exec dbh (format_string sth_select_user_rep [ml2int uid]) in
+      let s = Printf.sprintf "SELECT user_rep FROM wikitrust_user WHERE user_id = %s" (ml2int uid) in
+      let result = db_exec dbh s in
       match Mysql.fetch result with 
         | None -> raise DB_Not_Found
         | Some x -> not_null float2ml x.(0)
