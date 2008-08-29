4a4e52d7f85102802c7865cbc6ba4dab141e4687
Ian Pye <ipye@cs.ucsc.edu>
2008-08-28 23:04:26 -0700

    now a user can choose db engine

--- online/sql/create_wikitrust_tables-innodb.sql
+++ online/sql/create_wikitrust_tables-innodb.sql
@@ -14,7 +14,7 @@ CREATE TABLE wikitrust_global (
        rep_7			  float,
        rep_8			  float,
        rep_9			  float
-);
+) ENGINE=InnoDB;
 GRANT ALL ON wikitrust_global TO wikiuser;
 
 INSERT INTO wikitrust_global VALUES (0,0,0,0,0,0,0,0,0,0,0);
@@ -23,7 +23,7 @@ CREATE TABLE wikitrust_page (
        page_id             int PRIMARY KEY,
        deleted_chunks      longtext,
        page_info	   text NOT NULL
-);
+) ENGINE=InnoDB;
 GRANT ALL ON wikitrust_page TO wikiuser;
 
 CREATE TABLE wikitrust_revision (
@@ -31,14 +31,14 @@ CREATE TABLE wikitrust_revision (
         quality_info		text NOT NULL, 
 	reputation_delta        float DEFAULT 0.0,
 	overall_trust           float DEFAULT 0.0
-);
+) ENGINE=InnoDB;
 GRANT ALL ON wikitrust_revision TO wikiuser;
 
 CREATE TABLE wikitrust_colored_markup (
         revision_id     int PRIMARY KEY,
         revision_text   longtext NOT NULL,
 	revision_createdon varchar(32) NOT NULL
-);
+) ENGINE=InnoDB;
 GRANT ALL ON wikitrust_colored_markup TO wikiuser;
 
 CREATE TABLE wikitrust_sigs (
@@ -47,13 +47,13 @@ CREATE TABLE wikitrust_sigs (
        trust            longtext NOT NULL,
        origin           longtext NOT NULL,
        sigs     	longtext NOT NULL
-);
+) ENGINE=InnoDB;
 GRANT ALL ON wikitrust_sigs TO wikiuser;
 
 CREATE TABLE wikitrust_user (
        user_id     int PRIMARY KEY   ,
        user_rep    float DEFAULT 0.0
-);
+) ENGINE=InnoDB;
 GRANT ALL ON wikitrust_user TO wikiuser;
 
 COMMIT;
