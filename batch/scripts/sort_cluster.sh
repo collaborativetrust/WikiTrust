a652ba39f6fa24661b163371f821fd3331a7201b
ipye <ipye@cs.ucsc.edu>
2008-10-24 13:40:20 -0700

    added a temp dir to sort in

--- batch/scripts/sort_cluster.sh
+++ batch/scripts/sort_cluster.sh
@@ -3,12 +3,13 @@
 WORKING_DIR=$1
 OUTPUT_BASE=$2
 START=$3
+TEMP_DIR=$4
 
 count=0
 
 PROC_LIM=7
 OUTPUT="$OUTPUT_BASE$count"
-COMMAND_BASE="sort -n -k 2,2 -o $OUTPUT -m "
+COMMAND_BASE="sort -n -k 2,2 -o $OUTPUT -T $TEMP_DIR -m "
 COMMAND=$COMMAND_BASE
 NUM_TO_SORT=100
 
@@ -20,7 +21,7 @@ for file in `ls $WORKING_DIR`; do
     if [ "$num_added" -gt "$NUM_TO_SORT" ]; then
       $COMMAND &
       OUTPUT="$OUTPUT_BASE$count"
-      COMMAND_BASE="sort -n -k 2,2 -o $OUTPUT -m "
+      COMMAND_BASE="sort -n -k 2,2 -o $OUTPUT -T $TEMP_DIR -m "
       COMMAND=$COMMAND_BASE
       let num_added=0
     fi
