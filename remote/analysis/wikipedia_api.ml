3c7ad89932ddc5369c203315b6f9c725dd86bb30
B. Thomas Adler <thumper@ubuntu.(none)>
2009-05-02 14:31:16 -0700

    online_db: added access to latest rev_id for page

--- remote/analysis/wikipedia_api.ml
+++ remote/analysis/wikipedia_api.ml
@@ -3,7 +3,7 @@
 Copyright (c) 2009 The Regents of the University of California
 All rights reserved.
 
-Authors: Luca de Alfaro, Ian Pye
+Authors: Luca de Alfaro, Ian Pye, B. Thomas Adler
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
