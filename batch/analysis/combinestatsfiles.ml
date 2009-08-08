7e2fa4f4dc3c2f9d1850e226db85cd6a474a28a3
Luca de Alfaro <luca@dealfaro.org>
2009-08-08 02:21:48 -0700

    New bucketizing for stat sorting computation.

--- batch/analysis/combinestatsfiles.ml
+++ batch/analysis/combinestatsfiles.ml
@@ -40,66 +40,92 @@ let filetype = ".stats"
 
 let usage_message = "Usage: combinestats [<directory>]\nAll *.stats files in <directory> will be sorted\n"
 
-let dirname = ref ""
+let input_dir = ref ""
+let sorted_dir  = ref "SORTEDSTATS/"
+let bucket_dir = ref ""
 let outfile = ref ""
-let outdir  = ref "SORTEDSTATS/"
-let numbuckets = ref 2000
-let max_lines_in_mem = ref 200000
-
-let tempbuckets = Hashtbl.create 1750
-let sortedfilelist = ref ""
-  (* Get command line arguments *)
-let set_dirname (s: string) = dirname := s
-let set_outfile (s: string) = outfile := s
-let set_outdir  (s: string) = outdir  := s
-let set_numbuckets (s: string) = numbuckets := (int_of_string s)
-let set_max_lines_in_mem (n) = max_lines_in_mem := n
+let digits_per_bucket = ref 5
+let max_lines_in_mem = ref 1000000
+let do_sorting = ref false
+let use_dirs = ref false
 let lines_in_cache = ref 0
+let noop s = ()
 
 let command_line_format = [
-  ("-outfile", Arg.String (set_outfile),
+  ("-outfile", Arg.Set_string outfile,
   "write entire sorted output to this file (if empty, large output not produced)");
-  ("-outdir",  Arg.String (set_outdir),
+  ("-sorted_dir",  Arg.Set_string sorted_dir,
   "write split sorted files to this directory (defaults to ./SORTEDSTATS/)");
-  ("-n", Arg.String (set_numbuckets),
-  "number of buckets (default: 1750, limit: 9999)");
-  ("-flush", Arg.Int (set_max_lines_in_mem),
-  "number of lines to read into memory before flushing to disk (default: 5000)")
+  ("-input_dir",  Arg.Set_string input_dir,
+  "directory where the input files are");
+  ("-bucket_dir", Arg.Set_string bucket_dir,
+  "directory where the unsorted buckets are");
+  ("-n_digits", Arg.Set_int digits_per_bucket,
+  "number of digits that go into the same bucket (default: 5, or about a day)");
+  ("-use_subdirs", Arg.Set use_dirs, 
+  "Uses subdirectory structure for files to sort (default: false)");
+  ("-skip_sorting", Arg.Set do_sorting,
+  "Skips the sorting step of the files");
+  ("-flush", Arg.Set_int max_lines_in_mem,
+  "number of lines to read into memory before flushing to disk (default: 1000000)")
 ];;
   
   
-let _ = Arg.parse command_line_format set_dirname usage_message;;
+let _ = Arg.parse command_line_format noop usage_message;;
 
-if !dirname = "" then Arg.usage command_line_format usage_message;;
+(* There is Arg.Set but no Arg.Reset, hence this kludge *)
+do_sorting := not !do_sorting;;
 
-(* Create a temporary working directory *)
-try Unix.mkdir !outdir 0o740 with e -> begin print_string "Warning: output file directory already exists.\n"; flush stdout end;;
+(* Hash table where lines are stored *)
+let tempbuckets = Hashtbl.create 2000
 
-(* Create the temp files ("buckets") *)
-for i = 1 to !numbuckets do
-  let commandtext = "touch " ^ !outdir ^ (Printf.sprintf "/stats%04d.tstats" i) in 
-  ignore (Unix.system commandtext)
-done;;
+(* Create a temporary working directory tree *)
+try 
+  Unix.mkdir !bucket_dir 0o750;
+with e -> begin 
+  print_string "Warning: bucket directory already exists.\n"; 
+  flush stdout 
+end;
 
 (* Gets the list of files to sort *)
-let file_list_f = Unix.open_process_in ("find " ^ !dirname ^ " -name *" ^ filetype) in 
+let file_list_f = Unix.open_process_in ("find " ^ !input_dir ^ " -name *" ^ filetype) in 
 (* Waits a bit before reading from the pipe *)
 Unix.sleep 3;
 
 let flush_tmp () =
   (* Now writes the hashtable *)
   print_endline "Flushing";
-  for i = 1 to !numbuckets do begin 
-    if Hashtbl.mem tempbuckets i then begin 
-      let filename = !outdir ^ (Printf.sprintf "/stats%04d.tstats" i) in 
-      let file = open_out_gen [Open_append] 0o640 filename in
-      (* Gets all lines *)
-      let lines_list = Hashtbl.find_all tempbuckets i in 
-      let p (l: string) = begin output_string file l; output_string file "\n"; end in 
-      List.iter p lines_list;
-      close_out file;
-    end
-  end done;
+  (* This function is iterated on the hash table *)
+  let f k lines_list = 
+    (* The key of the hashtable is the index of the file to open. *)
+    (* We need to produce a filename. *)
+    let (f_name, d_name) = 
+      if !use_dirs then begin
+	let s = Printf.sprintf "%08d.bkt" k in 
+	let p1 = String.sub s 0 5 in
+	let d = !bucket_dir ^ "/" ^ p1 in
+	(d, d ^ "/" ^ s)
+      end else (Printf.sprintf "%s/%08d.bkt" !bucket_dir k, "")
+    in
+    let file = 
+      try 
+	open_out_gen [Open_append] 0o640 f_name
+      with Sys_error _ -> begin
+	(* If we use subdirs, make sure the subdir exists. *)
+	if !use_dirs then begin
+	  try
+	    Unix.mkdir d_name 0o750
+	  with Unix.Unix_error (Unix.EEXIST, _, _) -> ()
+	end;
+	(* Opens the file *)
+	open_out f_name
+      end
+    in
+    let p (l: string) = begin output_string file l; output_string file "\n"; end in 
+    List.iter p lines_list;
+    close_out file;
+  in 
+  Hashtbl.iter f tempbuckets;
   Hashtbl.clear tempbuckets;
   lines_in_cache := 0
 in
@@ -125,12 +151,14 @@ let bucketize_file () =
 	let idx1 = String.index line ' ' in
 	if (String.sub line 0 idx1) <> "Page:" then begin
 	  let idx2 = String.index_from line (idx1 + 1) ' ' in
-	  let k = min 4 (idx2 - idx1 - 6) in
-	  let time_piece = String.sub line (idx1 + 2) k in 
-	  let timestamp = int_of_string (time_piece) in
-	  let bound x = max 1 (min !numbuckets x) in 
- 	  let bucketnum = bound timestamp in
-	  Hashtbl.add tempbuckets bucketnum line;
+	  let k = idx2 - idx1 - !digits_per_bucket in
+	  let time_piece = String.sub line idx1 k in
+	  let bucketnum = int_of_string time_piece in
+	  if Hashtbl.mem tempbuckets bucketnum then begin
+	    let l = Hashtbl.find tempbuckets bucketnum in
+	    Hashtbl.remove tempbuckets bucketnum;
+	    Hashtbl.add tempbuckets bucketnum (line :: l)
+	  end else Hashtbl.add tempbuckets bucketnum [line];
 	  lines_in_cache := !lines_in_cache + 1;
 	  if !lines_in_cache >= !max_lines_in_mem then flush_tmp ()
 	end
@@ -147,22 +175,28 @@ with End_of_file -> ();
 flush_tmp ();
 ignore (Unix.close_process_in file_list_f);;
 
-(* Now sort each of the files *)
-for i = 1 to !numbuckets do begin 
-  let filename = !outdir ^ (Printf.sprintf "/stats%04d.tstats" i) in 
-  let outfilename = !outdir ^ (Printf.sprintf "/stats%04d.sorted" i) in 
-  let commandtext = ("sort -n -k 2,2 " ^ filename ^ " > " ^ outfilename) in
-  print_string ("Sorting: " ^ filename ^ "\n"); flush stdout;
-  ignore (Unix.system commandtext);
-  ignore (Unix.system ("rm " ^ filename))
-end done;;
 
-(* And concatenate all the sorted files together *)
-if !outfile <> "" then begin 
-  print_string "Concatenating the result into a single file...\n"; flush stdout; 
-  for i = 1 to !numbuckets do begin 
-    let filename = !outdir ^ (Printf.sprintf "/stats%04d.sorted" i) in 
-    sortedfilelist := (!sortedfilelist ^ " " ^ filename)
+if !do_sorting then begin
+(*
+  (* Now sort each of the files *)
+  for i = 1 to 10 do begin 
+    let filename = !bucket_dir ^ (Printf.sprintf "/stats%04d.tstats" i) in 
+    let outfilename = !sorted_dir ^ (Printf.sprintf "/stats%04d.sorted" i) in 
+    let commandtext = ("sort -n -k 2,2 " ^ filename ^ " > " ^ outfilename) in
+    print_string ("Sorting: " ^ filename ^ "\n"); flush stdout;
+    ignore (Unix.system commandtext);
+    ignore (Unix.system ("rm " ^ filename))
   end done;
-  ignore (Unix.system ("cat " ^ !sortedfilelist ^ " > " ^ !outfile))
+
+  (* And concatenate all the sorted files together *)
+  if !outfile <> "" then begin 
+    print_string "Concatenating the result into a single file...\n"; flush stdout; 
+    for i = 1 to !numbuckets do begin 
+      let filename = !outdir ^ (Printf.sprintf "/stats%04d.sorted" i) in 
+      sortedfilelist := (!sortedfilelist ^ " " ^ filename)
+    end done;
+    ignore (Unix.system ("cat " ^ !sortedfilelist ^ " > " ^ !outfile))
+  end
+ *)
 end;;
+
