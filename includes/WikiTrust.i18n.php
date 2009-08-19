8a2f3ac3e01108121c0a23a3c9302bbdee6dd3ef
B. Thomas Adler <thumper@ubuntu.(none)>
2009-08-19 16:33:35 -0700

    mwext: point to our new WikiTrust wiki

--- includes/WikiTrust.i18n.php
+++ includes/WikiTrust.i18n.php
@@ -11,7 +11,7 @@ $messages['en'] = array (
   |-
   | The article text is colored according to how much it has been revised.  An orange background indicates new, unrevised, text;  white is for text that has been revised by many reputed authors.  If you click on a word, you will be redirected to the diff corresponding to the edit where the word was introduced.  If you hover over a word, a pop-up displays the word author.
   |-
-  | The text color and origin are computed by [http://trust.cse.ucsc.edu/WikiTrust WikiTrust]; if you notice problems, you can submit a bug report [http://code.google.com/p/wikitrust/issues here].
+  | The text color and origin are computed by [http://wikitrust.soe.ucsc.edu/ WikiTrust]; if you notice problems, you can submit a bug report [http://code.google.com/p/wikitrust/issues here].
   |}',
 			 'wgNoTrustExplanation' => 
 			 '{| border="1" cellpadding="5" cellspacing="0" style="background:lightgreen; color:black"
