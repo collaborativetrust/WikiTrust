6ec15e70c7d5e7d0eb22424fe03c02fb6f9725c5
Ian Pye <ipye@zink.(none)>
2009-09-03 19:29:45 -0700

    ported python server to mod_perl

---
+++ remote/analysis/startup.pl
@@ -0,0 +1,25 @@
+use Apache::DBI;
+use DBI;
+use Cwd;
+use Compress::Zlib;
+use strict;
+
+unless (defined $ENV{MOD_PERL}){
+  die "\$ENV{MOD_PERL} not set!";
+}
+
+## ToDo Bo -- is there a better way to do this?
+use lib qw(/home/ipye/gits/wikitrust/remote/analysis);
+
+## Setup the DB.
+Apache::DBI->connect_on_init(
+  $ENV{WT_DBNAME},
+  $ENV{WT_DBUSER},
+  $ENV{WT_DBPASS},
+  {AutoCommit => 1}
+);
+
+## Require the WikiTrust module
+use WikiTrust;
+
+1;
