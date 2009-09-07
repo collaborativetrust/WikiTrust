78abde726dad41cd48ff222d7e70f5a1ba65b2ee
B. Thomas Adler <thumper@ubuntu.(none)>
2009-09-06 19:00:33 -0700

    modperl: some trivial cleanup of code

--- remote/analysis/WikiTrust.pm
+++ remote/analysis/WikiTrust.pm
@@ -4,16 +4,14 @@ package WikiTrust;
 #
 #
 
-use DBI;
 use strict;
+use warnings;
+use DBI;
 
-my $sleep_time_sec = 3;
-our $not_found_text_token = "TEXT_NOT_FOUND";
-our $find_revs_on_disk = 0;
+use constant SLEEP_TIME => 3;
+use constant NOT_FOUND_TEXT_TOKEN => "TEXT_NOT_FOUND";
+use constant FIND_REVS_ON_DISK => 0;
 
-sub get_not_found_text_token{
-  return $not_found_text_token;
-}
 
 sub mark_for_coloring {
   my ($page, $page_title, $dbh) = @_;
@@ -21,6 +19,9 @@ sub mark_for_coloring {
     "SELECT page_title FROM wikitrust_queue WHERE page_title = ? AND"
     . " processed <> 'processed'"
   );
+  # This doesn't seem safe.  What if another entry appears and gets
+  # marked as 'processing' between these two statements.  This could
+  # lead to multiple eval_online_wiki processes running, I think.
   my $ins_sth = $dbh->prepare(
     "INSERT INTO wikitrust_queue (page_id, page_title) VALUES (?, ?) " 
     . "ON DUPLICATE KEY UPDATE requested_on = now(), processed = 'unprocessed'"
@@ -65,13 +66,13 @@ sub get_median{
 
 sub fetch_colored_markup {
   my ($rev_id, $dbh) = @_;
-  if ($find_revs_on_disk){
-    return $not_found_text_token;
+  if (FIND_REVS_ON_DISK){
+    return NOT_FOUND_TEXT_TOKEN;
   } else {
     my $sth = $dbh->prepare ("SELECT revision_text FROM "
       . "wikitrust_colored_markup WHERE "
       . "revision_id = ?");
-    my $result = $not_found_text_token;
+    my $result = NOT_FOUND_TEXT_TOKEN;
     my $rv = $sth->execute(($rev_id));
     if ($rv && (my $ref = $sth->fetchrow_hashref())){
       my $median = get_median($dbh);
@@ -91,22 +92,18 @@ sub handle_text_request {
   my ($rev, $page, $user, $time, $page_title, $dbh) = @_;
   
   my $result = fetch_colored_markup($rev, $dbh);
-  if ($result eq $not_found_text_token){
+  if ($result eq NOT_FOUND_TEXT_TOKEN){
     # If the revision is not found among the colored ones,
     # we mark it for coloring,
     # and it wait a bit, in the hope that it got colored.
     mark_for_coloring($page, $page_title, $dbh);
-    sleep($sleep_time_sec);
+    sleep(SLEEP_TIME);
     # Tries again to get it, to see if it has been colored.
     $result = fetch_colored_markup($rev, $dbh);
   }
 
-  if ($result eq $not_found_text_token){
-    # No: we will have to wait until it gets colored.  For now, we report not found.
-    return $not_found_text_token;
-  } else {
-    return $result;
-  }
+  # Text may or may not have been found, but it's all the same now.
+  return $result;
 }
 
 1;
