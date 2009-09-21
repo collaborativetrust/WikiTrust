e622b2e10c6e1ffb71fbfb1058a3540b872fd9ac
B. Thomas Adler <thumper@ubuntu.(none)>
2009-09-21 15:54:45 -0700

    mwext: don't mention hovering, since it's off by default

--- includes/WikiTrust.i18n.php
+++ includes/WikiTrust.i18n.php
@@ -9,7 +9,7 @@ $messages['en'] = array (
 			 'wgTrustExplanation' => 
 			 '{| border="1" cellpadding="5" cellspacing="0" style="background:lightgreen; color:black; margin-top: 10px; margin-bottom: 10px;" id="wt-expl"
   |-
-  | The article text is colored according to how much it has been revised.  An orange background indicates new, unrevised, text;  white is for text that has been revised by many reputed authors.  If you click on a word, you will be redirected to the diff corresponding to the edit where the word was introduced.  If you hover over a word, a pop-up displays the word author.
+  | The article text is colored according to how much it has been revised.  An orange background indicates new, unrevised, text;  white is for text that has been revised by many reputed authors.  If you click on a word, you will be redirected to the diff corresponding to the edit where the word was introduced.
   |-
   | The text color and origin are computed by [http://wikitrust.soe.ucsc.edu/ WikiTrust]; if you notice problems, you can submit a bug report [http://code.google.com/p/wikitrust/issues here].
   |}',
