5691f2af039423cff4c5e01b1e6feec183e33acf
Ian Pye <ipye@zink.(none)>
2009-09-30 13:01:08 -0700

    loads files in dictionary order, prints stats to a log file, uses a persistent db connection, also works with a single file now

--- test-scripts/load_db.sh
+++ test-scripts/load_db.sh
@@ -1,29 +1,55 @@
 #!/bin/bash
 
 sql_dir=$1
-db_user=$2
-db_name=$3
-db_pass=$4
+logfile=$2
+#db_user=$2
+#db_name=$3
+#db_pass=$4
 
-if [ -z $db_pass ]; then
-  echo "usage: ./load_db.sh sql_dir dbuser dbname dbpass"
+if [ -z $logfile ]; then
+  echo "usage: ./load_db.sh (sql_dir or sql_file) logfile | mysql -u dbuser dbname -p"
   exit 0
 fi
 
+start_tx="BEGIN"
 drop_page_idx="DROP INDEX wikitrust_page_title_idx ON wikitrust_page"
 drop_rev_idx="DROP INDEX wikitrust_revision_id_timestamp_idx ON wikitrust_revision"
 create_page_idx="CREATE INDEX wikitrust_page_title_idx ON wikitrust_page (page_title)"
 create_rev_idx="CREATE INDEX wikitrust_revision_id_timestamp_idx ON wikitrust_revision (page_id, time_string, revision_id)"
+end_tx="COMMIT"
 
 # drop the indicis
-`echo $drop_page_idx | mysql -u $db_user -p$db_pass $db_name`
-`echo $drop_rev_idx | mysql -u $db_user -p$db_pass $db_name`
+echo $drop_page_idx
+echo $drop_rev_idx
 
-for file in `find $sql_dir -name *.sql`; do
-  `cat $file | mysql -u $db_user -p$db_pass $db_name`
-done
+# how are we doing?
+cma=0
+files_seen=0
+
+if [ -f "$sql_dir" ]; then
+  cat $sql_dir
+else
+  for file in `find $sql_dir -name *.sql | sort -id`; do
+    stime=`date +%s`
+    echo $start_tx
+    size=$(stat -c%s "$file")
+    cat $file
+    echo $end_tx
+    etime=`date +%s`
+    let elapsed=$etime-$stime
+    if [ "$elapsed" -eq "0" ]; then
+      let elapsed=1
+    fi
+    let bytes_sec=$size/$elapsed
+    let cma=$files_seen*$cma
+    let cma=$bytes_sec+$cma
+    let files_seen=$files_seen+1
+    let cma=$cma/$files_seen
+    echo "bytes/sec: $bytes_sec (spot) $cma (cma) $file" >> $logfile
+  done
+fi
 
 # and re-create them
-`echo $create_page_idx | mysql -u $db_user -p$db_pass $db_name`
-`echo $create_rev_idx | mysql -u $db_user -p$db_pass $db_name`
+echo $create_page_idx
+echo $create_rev_idx
 
