b4e422439ff5c17bb11dcb0b72dab139c29b6f62
Bullwinkle J. Moose <thumper@ubuntu.(none)>
2010-02-21 15:51:23 -0800

    API: check for basic errors

--- remote/analysis/WikiTrust.pm
+++ remote/analysis/WikiTrust.pm
@@ -109,6 +109,8 @@ sub secret_okay {
 sub mark_for_coloring {
   my ($page, $page_title, $dbh) = @_;
 
+  confess "Illegal page_id for '$page_title'" if $page <= 0;
+
   my $sth = $dbh->prepare(
     "INSERT INTO wikitrust_queue (page_id, page_title, priority) VALUES (?, ?, ?)"
 	." ON DUPLICATE KEY UPDATE requested_on = now()"
@@ -119,6 +121,9 @@ sub mark_for_coloring {
 sub handle_vote {
   my ($dbh, $cgi, $r) = @_;
   my ($rev, $page, $user, $time, $page_title) = get_stdargs($cgi);
+
+  die "Illegal page_id from user '$user'" if $page <= 0;
+  die "Illegal rev_id from user '$user'" if $rev <= 0;
 
   $r->content_type('text/plain');
 
@@ -422,7 +427,7 @@ sub handle_sharehtml {
 
     my $myhtml = $cgi->param('myhtml') || '';
     my $revid = $cgi->param('revid') || 0;
-    if (($myhtml eq '') || ($revid == 0)) {
+    if (($myhtml eq '') || ($revid <= 0)) {
       $r->content_type('text/plain');
       $r->print('Thanks, but no thanks.');
       return Apache2::Const::OK;
